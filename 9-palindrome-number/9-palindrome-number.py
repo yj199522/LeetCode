9a6595cbeda2413aca47565d0d60c31e8cee086a
yj199522 <91401304+yj199522@users.noreply.github.com>
2022-09-15 21:35:09 -0400

    Time: 143 ms (13.35%), Space: 13.8 MB (59.51%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.py
+++ 9-palindrome-number/9-palindrome-number.py
@@ -1,15 +1,11 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        if 0<= x <=9:
-            return True
         if x < 0:
             return False
-        
-        result = 0
         temp = x
+        result = 0
         while temp:
-            result = result * 10 + temp%10
-            temp = temp//10
-        
+            val = temp % 10
+            result = result * 10 + val
+            temp = temp // 10
         return result == x
-        
