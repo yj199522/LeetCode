9c529726157e64588284062b87eda7e7f6d00ca9
yj199522 <91401304+yj199522@users.noreply.github.com>
2022-09-15 21:18:52 -0400

    Time: 92 ms (61.53%), Space: 14 MB (49.73%) - LeetHub

--- 6-zigzag-conversion/6-zigzag-conversion.py
+++ 6-zigzag-conversion/6-zigzag-conversion.py
@@ -1,23 +1,20 @@
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
-        if s is None or numRows is None:
-            return ''
-        if numRows == 1:
-            return s
-        rows = [ '' for _ in range(numRows) ]
-        i = 0
-        opposite = False
-        for j in s:
-            if i < numRows - 1 and opposite == False:
-                rows[i]+=j
-                i+=1
-            elif i >0:
-                rows[i]+=j
-                i-=1
-            if i == 0:
-                opposite = False
-            if i == numRows - 1:
-                opposite = True
-        return ''.join(rows)
+        rows = [''] * numRows
+        direction = 1
+        current = 0
         
-        
+        if numRows == 1: return s
+
+        for char in s:
+            rows[current] += char
+            if current == 0: direction = 1
+            if current == numRows - 1: direction = -1
+
+            current += direction
+
+        output = ''
+        for row in rows:
+            output += row
+
+        return output
