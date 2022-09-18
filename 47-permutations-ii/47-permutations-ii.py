baa8f9195258b0016e94cb258967cf842225810c
yj199522 <91401304+yj199522@users.noreply.github.com>
2022-09-17 23:41:44 -0400

    Time: 2977 ms (5.00%), Space: 14.2 MB (78.87%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.py
+++ 47-permutations-ii/47-permutations-ii.py
@@ -1,17 +1,18 @@
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
-        self_ans = []
-        self.permuteHelper(nums, 0, len(nums), self_ans)
-        return self_ans
+        self._ans = []
+        self.permutationHelper(nums,0,len(nums), self._ans)
+        return self._ans
     
-    def permuteHelper(self, nums, start, end, result):
+    def permutationHelper(self, nums, start, end, result):
         if start==end:
             if nums not in result:
                 result.append(list(nums))
-            return 
+            return
+        
         for i in range(start, end):
             arr = self.swap(nums, i, start)
-            self.permuteHelper(nums, start+1, end, result)
+            self.permutationHelper(nums, start+1,end, result)
             arr = self.swap(nums, i, start)
             
     def swap(self, nums, start, end):
@@ -19,4 +20,3 @@ class Solution:
             return nums
         nums[start], nums[end] = nums[end], nums[start]
         return nums
-        
