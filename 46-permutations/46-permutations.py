2a7eb393ec69a0b26ae63c2cbbb7a0d96ceb7779
yj199522 <91401304+yj199522@users.noreply.github.com>
2022-09-17 23:34:27 -0400

    Time: 63 ms (56.12%), Space: 14 MB (83.92%) - LeetHub

---
+++ 46-permutations/46-permutations.py
@@ -0,0 +1,22 @@
+class Solution:
+    def permute(self, nums: List[int]):
+        self_ans = []
+        self.permuteHelper(nums, 0, len(nums), self_ans)
+        return self_ans
+    
+    def permuteHelper(self, nums, start, end, result):
+        if start==end:
+            result.append(list(nums))
+            return 
+        for i in range(start, end):
+            arr = self.swap(nums, i, start)
+            self.permuteHelper(nums, start+1, end, result)
+            arr = self.swap(nums, i, start)
+            
+    def swap(self, nums, start, end):
+        if start == end:
+            return nums
+        nums[start], nums[end] = nums[end], nums[start]
+        return nums
+        
+        
