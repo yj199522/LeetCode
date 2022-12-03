30a2062f58609fc4fedacc0a0ba85d9f2e02cfce
yj199522 <91401304+yj199522@users.noreply.github.com>
2022-12-03 10:06:55 -0800

    Time: 85 ms (6.67%), Space: 13.9 MB (75.00%) - LeetHub

--- 0804-unique-morse-code-words/0804-unique-morse-code-words.py
+++ 0804-unique-morse-code-words/0804-unique-morse-code-words.py
@@ -1,16 +1,14 @@
 class Solution:
     def uniqueMorseRepresentations(self, words: List[str]) -> int:
-        morseCodeList = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+        morse_code_array = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
         if len(words) < 2:
             return len(words)
-        result = []
-        minValue = ord('a')
+        result = set()
         for word in words:
-            morseCodeString = ''
-            for letter in word:
-                indexToFindMorseCode = ord(letter) - minValue
-                morseCodeString+=morseCodeList[indexToFindMorseCode]
-            if morseCodeString not in result:
-                result.append(morseCodeString)
+            word = word.lower()
+            transformations = ""
+            for chr in word:
+                transformations += morse_code_array[ord(chr) - 97]
+            result.add(transformations)
         return len(result)
                 
