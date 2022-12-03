569f0339d47d930183d2be2a7bf61cc63a36a788
yj199522 <91401304+yj199522@users.noreply.github.com>
2022-12-02 20:43:26 -0800

    Time: 164 ms (91.65%), Space: 14.4 MB (40.90%) - LeetHub

--- 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.py
+++ 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.py
@@ -1,22 +1,40 @@
 class Solution:
     def findRestaurant(self, list1: List[str], list2: List[str]) -> List[str]:
-        result = []
-        minValue = float('inf')
-        mapping = {}
-        if len(list2) > len(list1):
-            list1, list2 = list2, list1
-        for index, word in enumerate(list1):
-            mapping[word] = index
-        for index, word in enumerate(list2):
-            if word not in mapping:
-                continue
-            sumValue = mapping[word] + index
-            if minValue >  sumValue:
-                minValue = sumValue
-                result = [word]
-            elif minValue == sumValue:
-                result.append(word)
+        # result = []
+        # minValue = float('inf')
+        # mapping = {}
+        # if len(list2) > len(list1):
+        #     list1, list2 = list2, list1
+        # for index, word in enumerate(list1):
+        #     mapping[word] = index
+        # for index, word in enumerate(list2):
+        #     if word not in mapping:
+        #         continue
+        #     sumValue = mapping[word] + index
+        #     if minValue >  sumValue:
+        #         minValue = sumValue
+        #         result = [word]
+        #     elif minValue == sumValue:
+        #         result.append(word)
+        hashmap = {}
+        for i in range(len(list1)):   #step 1
+            hashmap[list1[i]] = i
+        
+        res = []
 
+        minsum = float("inf")   #step 2
+        
+        for j in range(len(list2)):    #step 3
+            if list2[j] in hashmap:
+                Sum = j + hashmap[list2[j]]    #step 3a
+                
+                if Sum < minsum:   #step 3b
+                    minsum = Sum
+                    res = []
+                    res.append(list2[j])
+                elif Sum == minsum:     #step 3c
+                    res.append(list2[j])
+        return res
         # for index1, wordforlist1 in enumerate(list1):
         #     if wordforlist1 not in list2:
         #         continue
