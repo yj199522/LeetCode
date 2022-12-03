816d64d8acd908bebd8856d8648122c6ce0b1d05
yj199522 <91401304+yj199522@users.noreply.github.com>
2022-12-02 20:14:59 -0800

    Time: 36 ms (85.92%), Space: 13.9 MB (68.00%) - LeetHub

--- 0500-keyboard-row/0500-keyboard-row.py
+++ 0500-keyboard-row/0500-keyboard-row.py
@@ -1,34 +1,30 @@
 class Solution:
     def findWords(self, words: List[str]) -> List[str]:
         keyboardLetterAsPerRow = [
-            set("qwertyuiop"),
-            set("asdfghjkl"),
-            set("zxcvbnm")
+            "qwertyuiop",
+            "asdfghjkl",
+            "zxcvbnm"
         ]
-        return [
-            word for word in words if any([set(word.lower()) <= e for e in keyboardLetterAsPerRow])
-        ]
-#         result = []
+        result = []
         
-#         for word in words:
-#             row = 0
-#             typeWord = ''
-#             searchRow = -1
-#             for letter in word:
-#                 while row < len(keyboardLetterAsPerRow) and searchRow < 0:
-#                     if letter.lower() in keyboardLetterAsPerRow[row]:
-#                         searchRow = row
-#                     else:
-#                         row+=1
-#                 if letter.lower() not in keyboardLetterAsPerRow[searchRow] and searchRow != -1:
-#                     break
-#                 else:
-#                     typeWord+=letter
-#             if typeWord == word:
-#                 result.append(word)
+        for word in words:
+            row = 0
+            typeWord = ''
+            searchRow = -1
+            for letter in word:
+                while row < len(keyboardLetterAsPerRow) and searchRow < 0:
+                    if letter.lower() in keyboardLetterAsPerRow[row]:
+                        searchRow = row
+                    else:
+                        row+=1
+                if letter.lower() not in keyboardLetterAsPerRow[searchRow] and searchRow != -1:
+                    break
+                else:
+                    typeWord+=letter
+            if typeWord == word:
+                result.append(word)
                 
-#         return result
-        
+        return result
                         
                     
         
