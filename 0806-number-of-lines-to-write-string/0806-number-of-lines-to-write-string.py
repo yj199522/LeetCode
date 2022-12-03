dc24a8bcfdc88f5b34899be42b93c180f2e5e979
yj199522 <91401304+yj199522@users.noreply.github.com>
2022-12-03 11:35:47 -0800

    Time: 85 ms (6.23%), Space: 14 MB (20.38%) - LeetHub

--- 0806-number-of-lines-to-write-string/0806-number-of-lines-to-write-string.py
+++ 0806-number-of-lines-to-write-string/0806-number-of-lines-to-write-string.py
@@ -2,16 +2,13 @@ class Solution:
     def numberOfLines(self, widths: List[int], s: str) -> List[int]:
         result = []
         noOfLines = 1
-        start = 0
         total = 0
-        while start < len(s):
-            orderOfChar = ord(s[start]) - ord('a')
-            letterValue = widths[orderOfChar]
+        for letter in s:
+            letterValue = widths[ord(letter) - ord('a')]
             total+=letterValue
             if total > 100:
                 total = letterValue
                 noOfLines+=1
-            start+=1
         return [noOfLines, total]
                 
         
