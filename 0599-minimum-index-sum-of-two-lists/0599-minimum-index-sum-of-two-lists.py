8bfeb95105f7bf487948ae5450a17b7547eda0c4
yj199522 <91401304+yj199522@users.noreply.github.com>
2022-12-02 20:36:10 -0800

    Time: 961 ms (14.04%), Space: 14.4 MB (40.90%) - LeetHub

--- 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.py
+++ 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.py
@@ -3,7 +3,9 @@ class Solution:
         result = []
         minValue = float('inf')
         for index1, wordforlist1 in enumerate(list1):
-            if wordforlist1 in list2:
+            if wordforlist1 not in list2:
+                continue
+            elif wordforlist1 in list2:
                 sumIndexValue = list2.index(wordforlist1)+index1
                 if minValue >  sumIndexValue:
                     minValue = sumIndexValue
