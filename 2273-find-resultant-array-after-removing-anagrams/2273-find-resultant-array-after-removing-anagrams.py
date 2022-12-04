6dda9c293b4727d659c3a4a39cc6522ec72da882
yj199522 <91401304+yj199522@users.noreply.github.com>
2022-12-04 13:35:36 -0800

    Time: 65 ms (87.23%), Space: 14 MB (31.55%) - LeetHub

--- 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.py
+++ 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.py
@@ -1,13 +1,21 @@
 class Solution:
     def removeAnagrams(self, words: List[str]) -> List[str]:
-        left, right = 0, 1
-        while left < right and right < len(words):
-            sortLeftWord, sortrightWord = sorted(words[left]), sorted(words[right])
-            if sortLeftWord == sortrightWord:
-                words.pop(right)
-            else:
-                left, right = right, right + 1
+        # left, right = 0, 1
+        # while left < right and right < len(words):
+        #     sortLeftWord, sortrightWord = sorted(words[left]), sorted(words[right])
+        #     if sortLeftWord == sortrightWord:
+        #         words.pop(right)
+        #     else:
+        #         left, right = right, right + 1
+        # return words
+        start = 0
+        while start < len(words) -1:
+            if sorted(words[start]) == sorted(words[start+1]):
+                words.remove(words[start+1])
+                continue
+            start+=1
         return words
+                
             
             
         
