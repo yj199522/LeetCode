ff6d6f8a86502716783c7ccfa1f71305326d6312
yj199522 <91401304+yj199522@users.noreply.github.com>
2022-12-04 14:30:11 -0800

    Time: 48 ms (92.19%), Space: 14.1 MB (89.38%) - LeetHub

--- 1598-crawler-log-folder/1598-crawler-log-folder.py
+++ 1598-crawler-log-folder/1598-crawler-log-folder.py
@@ -1,12 +1,10 @@
 class Solution:
     def minOperations(self, logs: List[str]) -> int:
-        result = []
-        for log in logs:
-            countOfDots = log.count('.')
-            if countOfDots == 2:
-                if len(result):
-                    result.pop()
-            elif countOfDots == 0:
-                result.append(log.split('/')[0])
-        return len(result)
+        ops = 0
+        for i in logs:
+            if '.' not in i:
+                ops += 1
+            elif ops > 0 and i == '../':
+                ops -= 1
+        return ops
         
